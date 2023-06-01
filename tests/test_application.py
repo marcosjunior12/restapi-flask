85f631dbbe84784337cedce5a21a9c7cf5ad87df
Marcos Junior <juninho1_2010@hotmail.com>
2023-06-01 14:19:57 -0300

    test: testing post

--- tests/test_application.py
+++ tests/test_application.py
@@ -9,6 +9,35 @@ class TestApplication():
         app = create_app('config.MockConfig')
         return app.test_client()
 
+    @pytest.fixture
+    def valid_user(self):
+        return {
+            "first_name": "marcos",
+            "last_name": "jr",
+            "cpf": "400.077.560-03",
+            "email": "teste",
+            "birth_date": "1998-07-04"
+          }
+
+    @pytest.fixture
+    def invalid_user(self):
+        return {
+            "first_name": "marcos",
+            "last_name": "jr",
+            "cpf": "400.077.560-06",
+            "email": "teste",
+            "birth_date": "1998-07-04"
+          }
+
     def test_get_users(self, client):
         response = client.get('/users')
         assert response.status_code == 200
+
+    def test_post_user(self, client, valid_user, invalid_user):
+        response = client.post('/user', json=valid_user)
+        assert response.status_code == 200
+        assert b"criado" in response.data
+
+        response = client.post('/user', json=invalid_user)
+        assert response.status_code == 400
+        assert b"CPF INVALID" in response.data
